ad3ab97a55ab9f13b511b325c632b3a7fcf2da45
Abbos Aliboev <132249275+abbosaliboev@users.noreply.github.com>
2024-11-01 18:01:29 +0900

    Update Folders for React

---
+++ Plantopia/src/App.js
@@ -0,0 +1,21 @@
+// src/App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
