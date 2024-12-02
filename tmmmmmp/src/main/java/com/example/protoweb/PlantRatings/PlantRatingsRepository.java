b8b80012a1df2749e6d60689900551eeda2ea28a
Taeyeong Kim <xodud7737@gmail.com>
2024-12-02 21:19:14 +0900

    hi

---
+++ tmmmmmp/src/main/java/com/example/protoweb/PlantRatings/PlantRatingsRepository.java
@@ -0,0 +1,7 @@
+package com.example.protoweb.PlantRatings;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlantRatingsRepository extends JpaRepository<PlantRatings, Integer> {
+
+}
---
+++ tmmmmmp/src/test/java/com/example/protoweb/ProtoWebSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.protoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtoWebSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
