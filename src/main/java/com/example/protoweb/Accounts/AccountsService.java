82855616aa4647d0628a907d30900dbf6b867e11
sspark603 <sspark1567@gmail.com>
2024-12-14 00:00:05 +0900

    1.0

---
+++ src/main/java/com/example/protoweb/Accounts/AccountsService.java
@@ -0,0 +1,49 @@
+package com.example.protoweb.Accounts;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AccountsService {
+	@Autowired
+    private AccountsRepository Sv;
+	
+	@Transactional(readOnly = true)
+	public List<Accounts> findAll() {
+		List<Accounts> acc = Sv.findAll();
+		return acc;
+	}
+	
+	@Transactional
+	public String save(String pass, String un, String nn) {
+		Sv.save(new Accounts(pass,un,nn));
+		return "Ok";
+	}
+	
+	@Transactional
+	public String save(Accounts ac) {
+		Sv.save(ac);
+		return "Ok";
+	}
+
+	@Transactional
+	public Accounts findById(int _id) {
+		Optional<Accounts> tmp = Sv.findById(_id);
+		return tmp.get();
+	}
+	
+	@Transactional
+	public String deleteById(int Id) {
+		Sv.deleteById(Id);
+		return "Ok";
+	}
+	
+	@Transactional(readOnly = true)
+	public boolean existsByUsername(String username) {
+		return Sv.existsByUsername(username);
+	}
+}
---
+++ src/main/java/com/example/protoweb/Plants/Plants.java
@@ -0,0 +1,80 @@
+package com.example.protoweb.Plants;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.example.protoweb.PlantRatings.PlantRatings;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Plants {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //자동으로 1씩 증가하면서 배정
+	private int id;
+
+	@Column
+	private String name;
+	
+	@Column
+	private int imgId;
+	
+	@Column
+	@Size(min=0,max=65535)
+	private String Description;
+	
+	@Column
+	@Size(min=0,max=65535)
+	private String feature;
+
+	@Column
+	private int RatingCount;
+
+	@Column
+	private long Rating;
+	
+	@Column
+	private double avgRating;
+	
+	@Column
+	private int price;
+	
+	@OneToMany(mappedBy="plant", cascade=CascadeType.ALL)
+	@OnDelete(action= OnDeleteAction.CASCADE)
+    private List<PlantRatings> plantratings = new ArrayList<>();
+	
+	@CreationTimestamp
+	private Timestamp created_at;
+	
+	public Plants() {
+		RatingCount = 0;
+		Rating = 0;
+		avgRating = 0;
+	};
+	public Plants(String _name,int _imgId, String _desc, String _feat, int _price) {
+		name = _name;
+		imgId = _imgId;
+		Description = _desc;
+		feature = _feat;
+		RatingCount = 0;
+		Rating = 0;
+		avgRating = 0;
+		price = _price;
+	}
+}
